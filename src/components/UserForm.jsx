abc3deb1f34799fcfbfaafc94b9e830afd958252
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-03-20 22:48:17 -0300

    update

--- src/components/UserForm.jsx
+++ src/components/UserForm.jsx
@@ -48,14 +48,14 @@ const UserForm = ({ setForm, getUsers, userSelected, setUserSelected }) => {
 
   return (
     <div className="w-screen h-screen fixed top-0 left-0 bg-black/80 flex items-center justify-center">
-      <div className=" w-2/5 h-3/5 bg-slate-300 rounded-lg p-8 shadow-lg shadow-black text-center flex flex-col relative">
+      <div className=" w-full h-4/5 mx-6 bg-slate-300/60 rounded-lg p-8 shadow-lg shadow-black text-center flex flex-col relative md:w-4/5 lg:w-3/5 lg:h-96 xl:h-4/5 xl:w-1/3 2xl:justify-evenly 2xl:gap-4">
         <button
           className="flex items-center justify-center absolute top-0 right-0 rounded-full w-8 H-8 bg-red-700 mr-2 mt-2 text-xl font-bold shadow-lg shadow-black cursor-pointer text-white border-2 border-white hover:cursor-pointer hover:transform hover:scale-125 hover:text-black"
           onClick={() => closeForm()}
         >
           X
         </button>
-        <h3 className="text-2xl font-bold">Formulario</h3>
+        <h3 className="text-2xl font-bold 2xl:text-3xl">Formulario</h3>
         <form
           className="flex flex-col gap-4 justify-center items-center mt-4"
           onSubmit={handleSubmit(submit)}
@@ -116,7 +116,7 @@ const UserForm = ({ setForm, getUsers, userSelected, setUserSelected }) => {
             />
           </div>
           <button
-            className=" bg-blue-500 text-white w-40 h-10 mt-4 rounded-md font-bold hover:bg-blue-700 hover:text-black hover:transform hover:scale-105 shadow-lg shadow-black"
+            className=" bg-blue-500 text-white w-40 h-10 rounded-md font-bold hover:bg-blue-700 hover:text-black hover:transform hover:scale-105 shadow-lg shadow-black 2xl:mt-6"
             type="submit"
           >
             {userSelected ? 'Actualizar' : 'Crear Usuario'}
--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -21,42 +21,34 @@ const UserList = ({ userList, setForm, selectUser, getUsers, getWarning }) => {
           <span className="text-2xl mr-1">+</span>Nuevo Usuario
         </button>
       </div>
-      <div
-        className="flex w-full flex-row flex-wrap gap-8 justify-evenly px-32 py-32"
-        style={{
-          backgroundImage: `url(${Background})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-        }}
-      >
+      <div className="flex w-full flex-row flex-wrap gap-8 justify-evenly px-4 py-4 sm:w-72 sm:h-96 sm:flex-col sm:gap-16 sm:justify-center sm:px-2 sm:py-2 md:items-center md:justify-center md:ml-6">
         {orderList.map((user) => (
           <div
-            className=" flex flex-row items-center justify-between p-4 flex-wrap w-72 h-60 bg-white/20 shadow-lg shadow-black rounded-lg"
+            className=" flex flex-col items-center justify-between p-4 flex-wrap w-full h-60 bg-white/20 shadow-lg shadow-black rounded-lg my-10 gap-0 sm:h-72 sm:gap-2 md:w-80 lg:w-4/5"
             key={user.id}
           >
-            <h4 className="w-full text-center pb-2 border-b-2 border-black font-black shadow-lg shadow-black rounded-lg">
+            <h4 className="w-full text-center py-2 border-b-2 border-black font-black shadow-lg shadow-black rounded-lg underline underline-offset-2">
               {user.first_name}, {user.last_name}
             </h4>
-            <div className="">
-              <p>
+            <div>
+              <p className="font-medium">
                 <strong className="underline-offset-2 underline font-bold">
                   Email:{' '}
                 </strong>{' '}
                 <br />
                 {user.email}
               </p>
-              <p>
-                <strong className="underline-offset-2  underline font-bold">
+              <p className="font-medium">
+                <strong className="underline-offset-2 underline font-bold">
                   Fecha de Nacimiento:{' '}
                 </strong>{' '}
                 <br />
                 {user.birthday}
               </p>
             </div>
-            <div className="flex items-center justify-center flex-row gap-4 rounded-md sm:flex-col">
+            <div className="flex items-center justify-center flex-row gap-20 rounded-md sm:gap-24 sm:mt-2">
               <div
-                className="bg-blue-500 text-white flex items-center justify-center flex-col gap-6 w-10 h-10 border-2 border-black rounded-md shadow-md shadow-black hover:bg-blue-700 hover:transform hover:scale-110 hover:text-black"
+                className="bg-blue-500 text-white flex items-center justify-center flex-col gap-12 w-10 h-10 border-2 border-black rounded-md shadow-md shadow-black hover:bg-blue-700 hover:transform hover:scale-110 hover:text-black z-0"
                 onClick={() => selectUser(user)}
               >
                 <i className="bx bxs-edit-alt bx-sm cursor-pointer"></i>
