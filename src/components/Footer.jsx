27b11b8d3e353017dabf0a541f1d88c4224cf711
Fernando Rodriguez <ferchulobo2015@gmail.com>
2023-03-21 10:34:41 -0300

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="w-full bg-white/30 shadow-lg shadow-black h-44 flex items-center flex-col justify-center gap-4 text-black font-bold">
+    <div className="w-full bg-white/30 shadow-lg shadow-black h-auto flex items-center flex-col justify-center gap-4 text-black font-bold">
       <p className="text-xl">Ferchulobo777 || Design</p>
       <p className="text-2xl">Fernando Gabriel Rodriguez</p>
       <p className="text-2xl">Academlo Generacion 23-1</p>
